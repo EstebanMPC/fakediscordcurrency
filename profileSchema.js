02b53eedf2c478e44735e963cf421796a9d82c4d
Esteban Porras <estebanporras@Estebans-MBP.fios-router.home>
2021-07-06 14:16:01 -0400

    start

--- profileSchema.js
+++ profileSchema.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const profileSchema = new mongoose.Schema({
     userID: {type: String, require: true, unique: true}
     serverID: {type: String, require: true}
+    // name of coin // nombre de la moneda//
     coins: {type Number, default: 1000}
     bank: {type: Number}
 })
